e3dc16f72163ff6685a6ac5dc85e2f6ac366a366
Jochen Schultz <jschultz@php.net>
2025-01-12 02:35:49 +0100

    fix dependencies

--- src/Neo4jBundle/Service/Neo4jService.php
+++ src/Neo4jBundle/Service/Neo4jService.php
@@ -1,21 +1,26 @@
 <?php
 
-namespace src\Neo4jBundle\Service;
+namespace IntelligentIntern\Neo4jBundle\Service;
 
-use GraphAware\Neo4j\Client\ClientBuilder;
+use Neoxygen\NeoClient\ClientBuilder;
 
 class Neo4jService implements GraphDBServiceInterface
 {
-   private $client;
+    private $client;
 
-   public function __construct()
-   {
-       $neo4jUrl = $_ENV['NEO4J_URL'] ?? throw new \RuntimeException('NEO4J_URL is not set');
-       $username = $_ENV['NEO4J_USERNAME'] ?? throw new \RuntimeException('NEO4J_USERNAME is not set');
-       $password = $_ENV['NEO4J_PASSWORD'] ?? throw new \RuntimeException('NEO4J_PASSWORD is not set');
+    public function __construct()
+    {
+        $neo4jUrl = $_ENV['NEO4J_URL'] ?? throw new \RuntimeException('NEO4J_URL is not set');
+        $username = $_ENV['NEO4J_USERNAME'] ?? throw new \RuntimeException('NEO4J_USERNAME is not set');
+        $password = $_ENV['NEO4J_PASSWORD'] ?? throw new \RuntimeException('NEO4J_PASSWORD is not set');
 
-       $this->client = ClientBuilder::create()
-           ->addConnection('default', "http://$username:$password@$neo4jUrl")
-           ->build();
-   }
+        $this->client = ClientBuilder::create()
+            ->withCredentials($neo4jUrl, $username, $password)
+            ->build();
+    }
+
+    public function createNode(array $properties): void
+    {
+        $this->client->run('CREATE (n:Node {props})', ['props' => $properties]);
+    }
 }
