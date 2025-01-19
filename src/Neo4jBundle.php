e4ce5c18c6aef277c677cd2c515fc59d3f74c3f2
Jochen Schultz <jschultz@php.net>
2025-01-19 15:30:34 +0100

    Update namespaces and docblocks for v1.0.0

--- src/Neo4jBundle.php
+++ src/Neo4jBundle.php
@@ -8,9 +8,8 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class Neo4jBundle extends AbstractBundle
 {
-   public function build(ContainerBuilder $container): void
-   {
-       parent::build($container);
-       $container->addCompilerPass(new GraphDBServiceCompilerPass());
-   }
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import(__DIR__ . '/../config/services.yaml');
+    }
 }
