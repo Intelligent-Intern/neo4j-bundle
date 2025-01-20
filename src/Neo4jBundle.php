1f105ae31b43e0647ddfe37850d78cab25eb2c55
Jochen Schultz <jschultz@php.net>
2025-01-20 03:35:33 +0100

    Update namespaces and docblocks for v1.0.0

--- src/Neo4jBundle.php
+++ src/Neo4jBundle.php
@@ -2,14 +2,23 @@
 
 namespace IntelligentIntern\Neo4jBundle;
 
+use App\DependencyInjection\Compiler\GraphDBStrategyCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class Neo4jBundle extends AbstractBundle
 {
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import(__DIR__ . '/../config/services.yaml');
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new GraphDBStrategyCompilerPass());
     }
 }
